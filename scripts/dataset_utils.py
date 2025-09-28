9fdfaeec957d1136ed5387eb1a3be1be9fe5be17
infiniteperplexity <infinite.perplexity@gmail.com>
2025-09-27 20:25:48 -0700

    found some interesting results for the 'young men' analysis

--- scripts/dataset_utils.py
+++ scripts/dataset_utils.py
@@ -1,5 +1,7 @@
 import pandas as pd
+import numpy as np
 from matplotlib import pyplot as plt
+import statsmodels.formula.api as smf
 
 ## Recode values for a variable
 def map_variable(
@@ -92,3 +94,53 @@ def plot_weight_agg_over_time(
     plt.title(title)
     plt.xticks(df[time].unique())
     plt.show()
+
+
+def weighted_bar_chart(
+    df,
+    var,
+    weight="weight",
+    title = None,
+    groupby = None,
+    agg = weighted_mean,
+):
+    if groupby is None:
+        raise ValueError("Haven't figured out what to do for this.")
+    nomiss = df[df[groupby].notna() & df[var].notna()]
+    nomiss.groupby(groupby).apply(lambda x: weighted_mean(x, var, weight=weight)).plot(kind="bar")
+    plt.xlabel(groupby)
+    plt.ylabel(var)
+    if title:
+        plt.title(title)
+    else:
+        plt.title(f"Average {var} by {groupby}")
+    plt.show()
+
+
+def prepare_modeling_data(df, numeric=[], categorical=[]):
+    df = df[numeric + categorical].replace({pd.NA: np.nan}).dropna(subset=numeric + categorical)
+    for col in numeric:
+        df[col] = pd.to_numeric(df[col], errors='coerce').astype('float64')
+        span = df[col].max() - df[col].min()
+        df[col] = df[col]/span
+    for col in categorical:
+        df[col] = df[col].astype('category')
+    return df
+
+def run_model(
+    df,
+    dependent,
+    numeric = [],
+    categorical = [], # tuples of varname/reference
+):
+    df = prepare_modeling_data(df, numeric=[dependent] + numeric, categorical=[col for col,_ in categorical])
+    cat_str = [f"C({col}, Treatment('{ref}'))" for col, ref in categorical]
+    if len(df[dependent].value_counts()) == 2:
+        # If the dependent variable is binary, use logistic regression
+        model = smf.logit(formula=f"{dependent} ~ {' + '.join(numeric + cat_str)}", data=df, missing='drop')
+    else:
+        # If the dependent variable is not binary, use OLS regression
+        model = smf.ols(formula=f"{dependent} ~ {' + '.join(numeric + cat_str)}", data=df, missing='drop')
+    fitted = model.fit()
+    print(fitted.summary())
+    return fitted
